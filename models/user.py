41d5079195142605a099cab8ef9ddb38912afb80
Abrshhh <abreham.dessiem@gmail.com>
2024-01-16 12:29:52 -0800

    task 7 completed

--- models/user.py
+++ models/user.py
@@ -5,11 +5,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes
     Attributes:
         email: email address
